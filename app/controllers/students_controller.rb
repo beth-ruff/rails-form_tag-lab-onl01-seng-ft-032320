ff3453b17fb0d342285dc28b2c8b820a478fe86e
Beth Ruff <beth.ruff2@gmail.com>
2020-05-28 18:08:47 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,13 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
-
+  
+  def new 
+  end 
+  
+  def create 
+  end
+  
   def show
     @student = Student.find(params[:id])
   end
